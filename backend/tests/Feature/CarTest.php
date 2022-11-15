0ff0abc522fe753df3e9c3ffe92ee0294c48d65b
demon <kupchik.anarchy@gamail.com>
2022-11-15 09:11:33 +0300

    add test

--- backend/tests/Feature/CarTest.php
+++ backend/tests/Feature/CarTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Feature;
 
+use App\Models\Car;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -26,6 +28,26 @@ class CarTest extends TestCase
         $response->assertStatus(200);
         $this->assertDatabaseHas('cars', [
             'vin' => 'qweqweqwe',
+        ]);
+    }
+
+    public function test_change_user(){
+        $users = User::factory()->count(2)->create();
+
+        $car = Car::factory()->create([
+            'user_id'=>$users->first()->id,
+        ]);
+
+        $response = $this->put('/api/cars/'.$car->id, [
+            'brand'=> $car->brand,
+            'model'=>$car->model,
+            'vin'=>$car->vin,
+            'user_id'=>$users->last()->id,
+        ], ['accept'=>'application/json']);
+
+        $response->assertStatus(200);
+        $this->assertDatabaseHas('cars', [
+            'user_id' => $users->last()->id,
         ]);
     }
 }
